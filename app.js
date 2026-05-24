19bba8f822fd99fe71fabde0bf6d239069e9fc03
maena <maenage.maeda.maeda@gmail.com>
2026-05-24 15:15:46 +0900

    Improve browser compatibility

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ function showResults() {
 
 function escapeHtml(value) {
   return String(value)
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 form.addEventListener("submit", (event) => {
